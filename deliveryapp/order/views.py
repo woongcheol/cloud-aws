c5ed2fe8ad2793acc2bb50d95ccec14cdf54bed1
Woongcheol Shin <1994_100@naver.com>
2021-11-17 11:00:50 +0900

    add order

--- deliveryapp/order/views.py
+++ deliveryapp/order/views.py
@@ -30,8 +30,9 @@ def menu(request, shop):
         # serializer = MenuSerializer(menu, many=True)
         # return JsonResponse(serializer.data, safe=False)
 
-        return render(request, 'order/menu_list.html', {'menu_list':menu})
+        return render(request, 'order/menu_list.html', {'menu_list':menu, 'shop':shop})
         
+# request.POST['address']
 
     elif request.method == 'POST':
         data = JSONParser().parse(request)
@@ -40,3 +41,25 @@ def menu(request, shop):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+from django.utils import timezone
+@csrf_exempt
+def order(request):
+    if request.method == 'POST':
+        address = request.POST['address']
+        shop = request.POST['shop']
+        food_list = request.POST.getlist('menu')
+        order_date = timezone.now()
+
+        shop_item = Shop.objects.get(pk=int(shop))
+
+        shop_item.order_set.create(address= address, order_date=order_date,shop=int(shop))
+        
+        order_item = Order.objects.get(pk = int(shop_item.order_set.latest('id').id))
+        for food in food_list:
+            order_item.orderfood_set.create(food_name = food)
+
+        return HttpResponse(status=200)
+    elif request.method =='GET':
+        order_list = Order.objects.all()
+        return render(request, 'order/order_list.html', {'order_list':order_list})
