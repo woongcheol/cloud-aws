0cb26934d7deb1b54789e52aebd4ac705ee533c1
Woongcheol Shin <1994_100@naver.com>
2021-11-20 20:55:14 +0900

    update timeinput

--- deliveryapp/boss/urls.py
+++ deliveryapp/boss/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from boss import views
 
 urlpatterns = [
-    path('timeinput/', views.time_input, name="timeinput"),
+    path('orders/<int:shop>', views.order_list, name="order_list"),
+    path('timeinput/', views.time_input, name="timeinput")
     #path('menus/<int:shop>', views.menu, name="menu"),
     #path('order/', views.order, name="order")
 ]
--- deliveryapp/boss/views.py
+++ deliveryapp/boss/views.py
@@ -6,13 +6,18 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 
 @csrf_exempt
-def time_input(request):
+def order_list(request, shop):
     if request.method =='GET':
-        order_list = Order.objects.all()
+        order_list = Order.objects.filter(shop=shop)
         return render(request, 'boss/order_list.html', {'order_list': order_list})
 
-    elif request.method =='POST':
+@csrf_exempt
+def time_input(request):
+    if request.method =='POST':
         order_item = Order.objects.get(pk=int(request.POST['order_id']))
+        shop = order_item.shop.id
         order_item.estimated_time = int(request.POST['estimated_time'])
         order_item.save()
-        return render(request, 'boss/success.html')
+        return render(request, 'boss/success.html',{'shop':int(shop)})
+    else:
+        return HttpResponse(status=404)
